d0637093fb3454c59c45c280094de9e32a191501
Vishal kumar <vishal.oneto11@gmail.com>
2023-03-27 13:39:42 +0530

    section4 done .

--- src/app/myComponents/recipes/recipeslist/recipeslist.component.ts
+++ src/app/myComponents/recipes/recipeslist/recipeslist.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
+
+// <==============================>
 
 @Component({
   selector: 'app-recipeslist',
   templateUrl: './recipeslist.component.html',
   styleUrls: ['./recipeslist.component.css']
 })
-export class RecipeslistComponent {
+
+// <================================>
+export class RecipeslistComponent implements OnInit {
+  myfirstRecipe: Recipe[] = [
+    new Recipe("myfirstdish", "my first model dish that we created", "http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_brown.png"),
+    new Recipe("myfirstdish", "my first model dish that we created", "http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_brown.png"),
+    new Recipe("myfirstdish", "my first model dish that we created", "http://commondatastorage.googleapis.com/codeskulptor-assets/lathrop/nebula_brown.png")
+  ];
+
+
+
+  // <-------------------------------->
+  constructor() {
+
+  }
+  // <-------------------------------->
+  ngOnInit(): void {
+
+  }
 
 }
--- src/app/myComponents/shoppinglist/shoppinglist.component.ts
+++ src/app/myComponents/shoppinglist/shoppinglist.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shoppinglist',
   templateUrl: './shoppinglist.component.html',
   styleUrls: ['./shoppinglist.component.css']
 })
-export class ShoppinglistComponent {
+export class ShoppinglistComponent implements OnInit {
+  ingredent: Ingredient[] = [
+    new Ingredient("Apples", 500),
+    new Ingredient("Oranges", 100),
+    new Ingredient("Mangoes", 200),
+    new Ingredient("Banana", 500),
+    new Ingredient("Payaya", 800),
+  ];
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+
+  }
 
 }
